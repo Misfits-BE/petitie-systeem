4b3230cbeca8fea6510274118bcf93249b3617a4
Tim Joosten <Tjoosten3@gmail.com>
2018-02-07 14:56:33 +0100

    implement Helpdesk admin navigation partial

--- resources/views/admin/helpdesk/index.blade.php
+++ resources/views/admin/helpdesk/index.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12"> {{-- Helpdesk navigation --}}
+                @include('shared.helpdesk.navigation')
+            </div> {{-- /// END helpdesk navigation --}}
+
+            <div class="col-md-8"> {{-- content --}}
+                {{-- /// Content --}}
+            </div> {{-- /// Content --}}
+
+        </div>
+    </div>
 @endsection
