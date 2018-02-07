857fb140f36ed16a7040713ca44128ba9e5228fc
Tim Joosten <Tjoosten3@gmail.com>
2018-02-07 12:57:37 +0100

    Register font-awesome as local npm dependency

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,6 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
 </head>
 <body>
     <div id="app" class="content-bottom">
