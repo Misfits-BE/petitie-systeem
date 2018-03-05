c2b479b19cbc33476371bc839f3a48f80bca8159
Tim Joosten <Tjoosten3@gmail.com>
2018-03-05 03:04:59 +0100

    Register petition signature store logic

--- app/Http/Controllers/Frontend/SignatureController.php
+++ app/Http/Controllers/Frontend/SignatureController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Misfits\Http\Controllers\Controller;
 use Misfits\Repositories\SignatureRepository;
+use Misfits\Http\Requests\Frontend\SignatureValidator;
+use Symfony\Component\HttpFoundation\Response;
+use Misfits\Repositories\PetitionRepository;
 
 /**
  * Class SignatureController 
@@ -16,21 +19,42 @@ use Misfits\Repositories\SignatureRepository;
  */
 class SignatureController extends Controller
 {
-    /** @var \Misfits\Repositories\SignatureRepository $signatures */
-    private $signatures; 
+    /** @var \Misfits\Repositories\SignatureRepository $signature */
+    private $signature; 
+
+    /** @var \Misfits\Repositories\PetitionRepository $petition */
+    private $petition;
 
     /**
-     * SignatureController constrcutor 
+     * SignatureController Constructor 
      * 
+     * @param  SignatureRepository $signature   DB wrapper for the petition signature logic
+     * @param  PetitionRepository  $petition    DB wrapper for the petition logic.
      * @return void
      */
-    public function __construct(SignatureRepository $signatures) 
+    public function __construct(SignatureRepository $signature, PetitionRepository $petition) 
     {
-        //
+        $this->signature = $signature; 
+        $this->petition  = $petition;
     }
 
-    public function store(): RedirectResponse 
+    /**
+     * Store a new signature in the database. 
+     * 
+     * @todo Implement phpunit test 
+     * @todo Register phpunit test 
+     * @todo Register validation class
+     * 
+     * @param  SignatureValidator $input The given user input (validated)
+     * @param  string             $slug  The unique identifier in the storage from the petition. 
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(SignatureValidator $input, string $slug): RedirectResponse 
     {
+        if ($this->signature->store($input->all(), $petition)) {
+            flash('You have signed the petitions.')->success();
+        }
 
+        return back(Response::HTTP_FOUND);
     }
 }
